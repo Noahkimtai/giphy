daf6d173a08cb48819380d17ff27e7f3daf710df
Noahkimtai <noahkimtai24@gmail.com>
2022-11-21 00:49:29 +0300

    initial commit

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GifList from './GifList'
+import GifSearch from './GifSearch'
+
+function GifListContainer(){
+    return(
+        <div>
+            <GifSearch />
+            <GifList />
+        </div>
+
+    )
+}
+export default GifListContainer;
---
+++ src/components/GifSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GifSearch(){
+    return(
+        <div>
+            SearchBar
+        </div>
+    )
+}
+
+export default GifSearch;
